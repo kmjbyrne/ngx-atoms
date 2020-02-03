808ca1a0dceb00251ce7780b4c67581d67b647c5
Keith Byrne <kmjbyrne@amazon.com>
2020-02-03 22:19:12 +0000

    changes

--- projects/atoms/src/lib/menu/menu.component.ts
+++ projects/atoms/src/lib/menu/menu.component.ts
@@ -41,31 +41,12 @@ export class AtomMenuComponent implements OnInit {
                 route: 'test'
             }
         ];
-        this.setLinkEventListeners();
     }
 
     toggle() {
         this.isToggled = !this.isToggled;
     }
 
-    clearToggle(elem) {
-        elem.classList.remove('navbar--expand');
-    }
-
-    toggleCallback(elem) {
-        elem.classList.toggle('navbar--expand');
-    }
-
-    setLinkEventListeners() {
-        const elements = [].slice.call(document.getElementsByClassName('navbar__parentlink'));
-        // tslint:disable-next-line: prefer-for-of
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].addEventListener('click', () => {
-                // elements.filter(item => item != elements[i]).map(item => this.clearToggle(item));
-                this.toggleCallback(elements[i]);
-            });
-        }
-    }
 
     getRoute(link: string, child = false): string {
         return child ? `${link}/${child}` : `${link}`;
